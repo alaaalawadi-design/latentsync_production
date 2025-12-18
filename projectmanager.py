61a187c2db8564795474abc6d2df0f44a65d875f
alaaalawadi-design <alaa.alawadi@ad.cyshield>
2025-12-18 01:04:30 +0000

    last commit

--- projectmanager.py
+++ projectmanager.py
@@ -24,7 +24,7 @@ class ProjectManager:
         self.whisper_small_model_path = self.models_dir / "whisper" / "small.pt"
         self.whisper_tiny_model_path = self.models_dir / "whisper" / "tiny.pt"
         self.detector_path = self.models_dir 
-        self.ref_video_path = self.data_dir / "ref_videos" / "ref_video.mp4"
+        self.ref_video_path = self.data_dir / "ref_videos" / "1.mp4"
         self.saved_ref_video_data_path = self.data_dir / "ref_videos" / "ref_frames_data.pkl"
         self.silent_video_path = self.data_dir / "ref_videos" / "silent.mp4" 
 
---
+++ unit_tests/test2.py
@@ -0,0 +1,75 @@
+
+import sys
+from pathlib import Path
+
+# ----- IMPORT AI ----------
+sys.path.append('/home/administrator/disk2/alaa/GeneAI2/latentsync_production/cysync')
+import main
+
+# --------- MODEL LOADING ----------
+from projectmanager import ProjectManager
+
+base_dir = Path('/home/administrator/disk2/alaa/GeneAI2/latentsync_production/cysync')
+models_dir = Path('/home/administrator/disk2/alaa/GeneAI2/latentsync_production/checkpoints')
+project_manager = ProjectManager(base_dir, models_dir)
+
+lipsync_app = main.App(project_manager)
+
+
+
+# # # --------- MODEL INFERENCE ----------
+# audio_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/audios/audio1.wav"
+# save_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/results/test1/"
+# backgrounds = ['morning', 'night', 'noon', 'sunset']
+# project_manager.set_audio_path(audio_path)
+# project_manager.set_save_path(save_path)
+# project_manager.set_background_paths(backgrounds)
+# lipsync_app.run()    
+
+
+# audio_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/audios/audio2.wav"
+# save_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/results/test2/"
+# backgrounds = ['morning', 'night', 'noon', 'sunset']
+# project_manager.set_audio_path(audio_path)
+# project_manager.set_save_path(save_path)
+# project_manager.set_background_paths(backgrounds)
+# lipsync_app.run()    
+
+
+
+audio_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/audios/perfect1_2.wav"
+save_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/results/test3/"
+backgrounds = ['morning', 'night', 'noon', 'sunset']
+project_manager.set_audio_path(audio_path)
+project_manager.set_save_path(save_path)
+project_manager.set_background_paths(backgrounds)
+lipsync_app.run()    
+
+
+
+# audio_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/audios/Perfect_2_5.wav"
+# save_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/results/test4/"
+# backgrounds = ['morning', 'night', 'noon', 'sunset']
+# project_manager.set_audio_path(audio_path)
+# project_manager.set_save_path(save_path)
+# project_manager.set_background_paths(backgrounds)
+# lipsync_app.run()    
+
+
+
+# audio_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/audios/perfect_3_4.wav"
+# save_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/results/test5/"
+# backgrounds = ['morning', 'night', 'noon', 'sunset']
+# project_manager.set_audio_path(audio_path)
+# project_manager.set_save_path(save_path)
+# project_manager.set_background_paths(backgrounds)
+# lipsync_app.run()    
+
+
+# audio_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/audios/perfect4.wav"
+# save_path = "/home/administrator/disk2/alaa/GeneAI2/latentsync_production/test_data/results/test6/"
+# backgrounds = ['morning', 'night', 'noon', 'sunset']
+# project_manager.set_audio_path(audio_path)
+# project_manager.set_save_path(save_path)
+# project_manager.set_background_paths(backgrounds)
+# lipsync_app.run()    
